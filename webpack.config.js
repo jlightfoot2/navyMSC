02126140fb2ae540a91eeed1bebfd8b921691ae4
Daniel Gilfoy <dgilfoy@gmail.com>
2017-07-06 10:06:52 -0700

    cordova build attempt

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
+        publicPath: './'
     },
     devServer: {
         hot: true,
@@ -25,7 +25,7 @@ module.exports = {
         contentBase: path.resolve(__dirname, 'dist'),
         // match the output path
 
-        publicPath: '/'
+        publicPath: './'
         // match the output `publicPath`
     },
 
--- webpack.cordova.config.js
+++ webpack.cordova.config.js
@@ -14,7 +14,7 @@ module.exports = {
     output: {
         filename: "bundle.js",
         path: buildPath,
-        publicPath: '/'
+        publicPath: './'
     },
 
     // Enable sourcemaps for debugging webpack's output.
